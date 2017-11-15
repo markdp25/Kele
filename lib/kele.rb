6aef6bc00dde75cd76fa0f4a2f8af08b4f0c5f2a
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-11-15 18:48:51 -0500

    Current User added

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include 'httparty'
@@ -9,5 +10,10 @@ class Kele
     post_response = self.class.post('/sessions', body: { email: email, password: password})
     @user_auth_token = post_response['auth_token']
     raise "Email or Password is invalid. Please Try Again." if user_auth_token.nil?
+  end
+
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
+    @data_hash = JSON.parse(response.body)
   end
 end
