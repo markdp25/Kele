d6843553c22a94ee0d2fdd94615d6e5a371e84e5
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-11-20 19:47:57 -0500

    Add messaging

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,14 +3,15 @@ require 'json'
 require './lib/roadmap'
 
 class Kele
-  include 'httparty'
+  include HTTParty
+  include Roadmap
 
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
     post_response = self.class.post('/sessions', body: { email: email, password: password})
     @user_auth_token = post_response['auth_token']
-    raise "Email or Password is invalid. Please Try Again." if user_auth_token.nil?
+    raise "Email or Password is invalid. Please Try Again." if @user_auth_token.nil?
   end
 
   def get_me
@@ -27,6 +28,30 @@ class Kele
       end
     end
     puts availability
+  end
+
+  def get_messages(page = nil)
+        if page
+          response = self.class.get('/message_threads',
+            headers: { "authorization" => @user_auth_token },
+            body: { page: page })
+        else
+          response = self.class.get('/message_threads',
+            headers: { "authorization" => @user_auth_token })
+        end
+      JSON.parse(response.body)
+    end
+
+  def create_message(sender, recipient_id, token = nil, subject, stripped_text)
+    response = self.class.get('/messages',
+    headers: { "authorization" => @user_auth_token },
+    body: {
+      sender: sender,
+      recipient_id: recipient_id,
+      token: token,
+      subject: subject,
+      stripped_text: stripped_text
+      })
   end
 
 
